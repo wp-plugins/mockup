54c144e345df277348bb0f29e90b5c954171aea4
Eelco Tjallema <Eelco Tjallema@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-13 09:59:25 +0000

    Update mistake
    git-svn-id: https://plugins.svn.wordpress.org/mockup/tags/1.2.0@913201 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/view/single.php
@@ -0,0 +1,127 @@
+<?php 
+
+class Single extends MockUp {
+
+	public function __construct() {
+
+		$this->postID = get_the_ID();
+		$this->mockupID = get_post_meta($this->postID, '_mockup_id_1', true);
+		$this->mockupURL = get_permalink($this->mockupID);
+
+		$this->description = get_post_meta($this->postID, '_mockup_description_1', true);
+		$this->comments = get_post_meta($this->postID, '_mockup_comment_settings_1', true);
+		$this->terms = get_the_terms($this->postID, $this->taxonomy);
+
+		$this->mockup_single_check();
+		$this->mockup_single_layout();
+	}
+
+
+	public function mockup_single_check() {
+
+		if(empty($this->mockupID) && is_user_logged_in()) {
+
+			$title = __('No Mockup found' ,'MockUp');
+			$message = __('You did not add a MockUp.' ,'MockUp');
+
+			wp_die($message, $title);
+
+		} elseif(empty($this->mockupID) && !is_user_logged_in()) {
+
+			wp_redirect(get_bloginfo('url'));
+		}
+	}
+
+
+	public function mockup_single_layout() {
+
+		// Get image data.
+		$image_data = wp_get_attachment_image_src($this->mockupID, 'full');
+
+		$this->url = $image_data[0];
+		$this->height = $image_data[2];
+
+		// Get image position.
+		$this->position = get_post_meta($this->postID, '_mockup_position_1', true);
+
+		// Get colors.
+		$this->bgcolor = get_post_meta($this->postID, '_mockup_background_color_1', true);
+		$this->menucolor = get_post_meta($this->postID, '_mockup_slidebox_1', true);
+	}
+}
+
+
+$single = new Single; ?>
+
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+
+		<meta name="robots" content="noindex,nofollow" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
+
+		<title><?php wp_title('|', true, 'right'); ?></title>
+
+		<link href="<?php echo plugins_url('mockup'); ?>/include/css/single.css" rel="stylesheet" />
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script type="text/javascript">
+			post_id = '<?php echo $single->postID; ?>';
+			ajax_url = '<?php echo admin_url('admin-ajax.php'); ?>'
+		</script>
+		<script src="<?php echo plugins_url('mockup'); ?>/include/js/single.min.js"></script>
+
+	</head>
+
+	<body style="height: <?php echo $single->height.'px'; ?>; background-image: url('<?php echo $single->url; ?>'); background-position:<?php echo $single->position; ?> ; background-color: <?php echo $single->bgcolor; ?>;">
+
+		<div class="slidebox <?php if(!empty($single->menucolor)) echo 'slidebox_',$single->menucolor; else echo 'slidebox_light'; ?>">
+
+			<div class="content">
+
+				<a href="#" title="<?php _e('Close', 'MockUp'); ?>" id="close" class="show-title"><i class="fa fa-arrow-circle-left"></i></a>
+
+				<div id="load">
+
+					<div class="loading"></div>
+
+				</div>
+
+				<div class="navbar">
+
+					<?php
+
+					if(!empty($single->description)) {
+
+						echo '<a href="#" title="'.get_option('mockup_description_btn').'" id="description" class="toggle show-title"><i class="fa fa-pencil-square-o"></i></a>';
+					} 
+
+					if($single->comments == 'enable') {
+
+						echo '<a href="#" title="'.get_option('mockup_comment_btn').'" id="comment" class="toggle show-title"><i class="fa fa-comments-o"></i></a>';
+					} 
+
+					if($single->terms && !is_wp_error($single->terms)) {
+
+						echo '<a href="#" title="'.get_option('mockup_related_btn').'" id="related" class="toggle show-title"><i class="fa fa-list"></i></a>';
+
+					} ?>
+
+					<a href="#" title="<?php echo get_option('mockup_approve_btn'); ?>" id="approve" class="toggle show-title"><i class="fa fa-check-square-o"></i></a>
+
+				</div>
+				
+			</div>
+
+		</div>
+
+	</body>
+
+</html>
---
+++ uninstall.php
@@ -0,0 +1,44 @@
+<?php
+
+if(!defined('WP_UNINSTALL_PLUGIN')) {
+
+	exit();
+
+} else {
+
+	$options = get_option('mockup_uninstall');
+
+	// For Single site
+	if(!is_multisite()) {
+
+		// Loop through the options
+		foreach($options as $name) {
+			delete_option($name);
+		}
+
+		delete_option('mockup_related_popup_btn');
+		delete_option('mockup_uninstall');
+
+	// For Multisite
+	} else {
+
+		global $wpdb;
+		$blog_ids = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs");
+		$original_blog_id = get_current_blog_id();
+
+		foreach($blog_ids as $blog_id) {
+
+			switch_to_blog($blog_id);
+
+			// Loop through the options
+			foreach($options as $name) {
+				delete_option($name);
+			}
+
+			delete_option('mockup_related_popup_btn');
+			delete_option('mockup_uninstall');
+		}
+
+		switch_to_blog($original_blog_id);
+	}
+}
