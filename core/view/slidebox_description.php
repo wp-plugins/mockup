16c6f430925a2d92bdebeaf501555a2518b5752e
Eelco Tjallema <Eelco Tjallema@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-29 10:54:29 +0000

    Delete core file
    git-svn-id: https://plugins.svn.wordpress.org/mockup/trunk@1056180 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core/view/slidebox_description.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$id = filter_input(INPUT_POST, 'post_id', FILTER_SANITIZE_NUMBER_INT);
-
-	if(!empty($id)) {
-
-		echo '<h1>'.get_option('mockup_description_title').'</h1>';
-		echo '<p>'.nl2br(get_post_meta($id, '_mockup_description_1', true)).'</p>';
-	}
